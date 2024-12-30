bda0b9b6d78f09766ff67e15a79181f1a7b77b19
Calvin Nguyen <calvinnguyen03@gmail.com>
2024-12-29 19:30:07 -0800

    defined components

---
+++ my-portfolio/src/app/page.tsx
@@ -0,0 +1,16 @@
+import Header from "./Header";
+import Name from "./Name"
+
+export default function App() {
+  return (
+    <>
+    {/*<div><NavBar></NavBar></div> */}
+    <div><Header></Header></div>
+    <div><Name></Name></div>
+    {/*<div><About></About></div>
+    <div><Project name="1"></Project></div>
+    <div><Project name="2"></Project></div>
+    <div><Project name="3"></Project></div> */}
+    </>
+  )
+}
