610689ee08f65efa585cca9aae4aebbba171b183
Calvin Nguyen <calvinnguyen03@gmail.com>
2024-12-30 22:39:58 -0800

    changed to react-type-animation to support react18

--- my-portfolio/src/app/Text.tsx
+++ my-portfolio/src/app/Text.tsx
@@ -1,5 +1,5 @@
 "use client";
-import Typical from 'react-typical'
+import { TypeAnimation } from 'react-type-animation';
 
 type TextProps = {
     label: string;
@@ -10,12 +10,8 @@ export default function Text({ label }: TextProps) {
     if (label === "about") {
 
         return <div className="font-lato text-xl my-8">
-            <div className="flex mb-2">I'm Calvin Nguyen,
-            <Typical className="ml-1"
-            steps={[" an incoming new graduate 🎓!", 2000, " a software developer 💻!", 2000, "a coffee lover ☕!", 2000, "a gym go-er 🏋️!", 2000, "a basketball enjoyer 🏀!", 2000]}
-            loop={Infinity}
-            wrapper="p"
-            />
+            <div className="flex mb-2">I'm Calvin Nguyen,{" "}
+            <TypeAnimation sequence={[" an incoming new graduate 🎓!", 2000, " a software developer 💻!", 2000, "a coffee lover ☕!", 2000, "a gym go-er 🏋️!", 2000, "a basketball enjoyer 🏀!", 2000]} speed={50} repeat={Infinity} className="ml-1"></TypeAnimation>
             </div>
             <p>Currently a senior at UC Irvine pursuing a Bachelor's of Science in Computer Science, I have a deep passion for {" "}<span className="font-bold">crafting software solutions and intelligent systems that make a tangible impact on people’s lives.</span> My academic background has provided me with a strong foundation in Computer Science fundamentals, which I have applied to solve real-world challenges.</p>
             <p className="my-2">
